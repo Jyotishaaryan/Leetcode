b6ba91bb7c311687ab131f278066bb571db86e10
Jyotishaaryan <jyotishaaryan1402@gmail.com>
2026-06-27 13:18:12 +0530

    Time: 1 ms (2.98%), Space: 7.8 MB (70.56%) - LeetHub

---
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    long long M  = 1e9 + 7;
+    int solve(long long x , long long  n)
+    {
+        if(n == 0) return 1;
+        long halfpow = solve(x,n/2);
+        long long sqpow = halfpow * halfpow;
+
+        if(n<0)
+        {
+            return solve(1/x,-n);
+        }
+        if(n % 2 != 0)
+        {
+            return (x*sqpow) % M;
+        }
+        else
+        {
+            return sqpow % M;
+        }
+    }
+    int countGoodNumbers(long long n) {
+        return ((long long)solve(5,(n+1)/2)*solve(4,(n)/2))%M;
+    }
+};

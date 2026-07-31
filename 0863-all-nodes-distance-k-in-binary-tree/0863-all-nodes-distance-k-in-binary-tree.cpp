90429d6800a263b0f8e88aa1ddde804f671e9617
Jyotishaaryan <jyotishaaryan1402@gmail.com>
2026-07-31 12:35:16 +0530

    Time: 3 ms (81.8%), Space: 15.3 MB (84.29%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,57 +9,70 @@
  */
 class Solution {
 public:
-        unordered_map<TreeNode* , TreeNode*> par;
-        set<TreeNode*>s;
-        vector<int> ans;
-
-        void findParent(TreeNode* root)
-        {
-            if(root == NULL)
-            {
-                return;
-            }
-            if(root->left)
-            {
-                par[root->left] = root;
-            }
-            if(root->right)
-            {
-                par[root->right] = root;
-            }
-            findParent(root->left);
-            findParent(root->right);
-        }
-        void solve(TreeNode * node, int k)
-        {
-            if(s.find(node) != s.end())
-            {
-                return;
-            }
-            s.insert(node);
-            if(k==0)
-            {
-                ans.push_back(node->val);
-            }
-            if(node->left)
-            {
-                solve(node->left , k-1);
-            }
-            if(node->right)
-            {
-                solve(node->right , k-1);
-            }
-            if(par[node])
-            {
-                solve(par[node], k-1);
-            }
+    unordered_map<TreeNode*, TreeNode*> parent;
+    void addParent(TreeNode* root) {
+        if(!root)
             return;
-        }
-    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-            findParent(root);
-            solve(target,k);
-            return ans;
         
+        if(root->left)
+            parent[root->left] = root;
+        
+        addParent(root->left);
+        
+        if(root->right)
+            parent[root->right] = root;
+        
+        addParent(root->right);
+    }
+    
+    void collectKDistanceNodes(TreeNode* target, int k, vector<int>& result) {
+        
+        queue<TreeNode*> que;
+        que.push(target);
+        unordered_set<int> visited;
+        visited.insert(target->val);
+        
+        while(!que.empty()) {
+            
+            int n = que.size();
+            if(k == 0)
+                break;
+            
+            while(n--) {
+                TreeNode* curr = que.front();
+                que.pop();
+                
+                if(curr->left && !visited.count(curr->left->val)) {
+                    que.push(curr->left);
+                    visited.insert(curr->left->val);
+                }
+                if(curr->right && !visited.count(curr->right->val)) {
+                    que.push(curr->right);
+                    visited.insert(curr->right->val);
+                }
+                
+                if(parent.count(curr) && !visited.count(parent[curr]->val)) {
+                    que.push(parent[curr]);
+                    visited.insert(parent[curr]->val);
+                }
+            }
+            k--;
+        }
+        
+        while(!que.empty()) {
+            TreeNode* temp = que.front();
+            que.pop();
+            result.push_back(temp->val);
+        }
+    }
+    
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        vector<int> result;
+        
+        addParent(root);
+        
+        collectKDistanceNodes(target, k, result);
+        return result;
     }
 };
         

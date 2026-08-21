1eb6be85f7960da467928f6e644d53878d19af24
Jyotishaaryan <jyotishaaryan1402@gmail.com>
2026-08-21 23:54:47 +0530

    Time: 123 ms (68.99%), Space: 20.7 MB (46.51%) - LeetHub

---
+++ 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    long long findKthSmallest(vector<int>& coins, int k) {
+        int n = coins.size();
+
+        auto gcd = [](long long a, long long b) {
+            while (b) {
+                long long t = a % b;
+                a = b;
+                b = t;
+            }
+            return a;
+        };
+
+        auto lcm = [&](long long a, long long b) {
+            return a / gcd(a, b) * b;
+        };
+
+        
+        auto count = [&](long long x) {
+            long long ans = 0;
+
+            for (int mask = 1; mask < (1 << n); mask++) {
+                long long L = 1;
+                int bits = 0;
+
+                for (int i = 0; i < n; i++) {
+                    if (mask & (1 << i)) {
+                        bits++;
+                        L = lcm(L, coins[i]);
+
+                        
+                        if (L > x)
+                            break;
+                    }
+                }
+
+                if (L > x)
+                    continue;
+
+                if (bits & 1)
+                    ans += x / L;
+                else
+                    ans -= x / L;
+            }
+
+            return ans;
+        };
+
+        long long lo = 1;
+        long long hi = 1LL * k * (*min_element(coins.begin(),
+                                                coins.end()));
+
+        while (lo < hi) {
+            long long mid = lo + (hi - lo) / 2;
+
+            if (count(mid) >= k)
+                hi = mid;
+            else
+                lo = mid + 1;
+        }
+
+        return lo;
+    }
+};
